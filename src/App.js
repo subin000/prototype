42b545fcc7abafd46a4097d1bff1ce209e4220da
Subin <subin.suresh@somaiya.edu>
2024-10-26 19:44:55 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom';
+import axios from 'axios';
+import Sample from './Sample';
 
 function App() {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const authToken = localStorage.getItem('authToken');
+        if (!authToken) {
+          console.log("no token found")
+        }
+        const response = await axios.get('https://ser-2f05.onrender.com/user/profile', {
+          headers: { Authorization: `Bearer ${authToken}` },
+        });
+        setUserData(response.data);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+        if (error.response && error.response.status === 401) {
+          console.log('login');
+        }
+      }
+    };
+
+    fetchUserData();
+  }, []); 
+
+  const handleLogout = () => {
+    localStorage.removeItem('authToken');
+  };
+
   return (
-   <div>
-    hello
-   </div>
+    <>
+      <Router>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand as={Link} to="/home">Hotel Booking</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/home">Home</Nav.Link>
+              <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
+              <Nav.Link as={Link} to="/book">Bookings</Nav.Link>
+            </Nav>
+            <Nav className="justify-content-end">
+              {userData ? (
+                <>
+                <Nav.Item style={{color : "white"}}>Hello, {userData.name}</Nav.Item>
+                <Nav.Link as={Link} to="/login" onClick={handleLogout}>Logout</Nav.Link>
+                </>
+              ) : (
+                <>
+                  <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                  <Nav.Link as={Link} to="/signup">Signup</Nav.Link>
+                </>
+              )}
+            </Nav>
+          </Container>
+        </Navbar>
+
+        <Routes>
+          <Route path="/" element={<sample/>}></Route>
+          <Route path="/book" element={<Sample />} />
+          <Route path="/home" default element={<Sample />} />
+          <Route path="/bookpage" element={<Sample />} />
+          <Route path="/profile" element={<Sample />} />
+          <Route path="/login" element={<Sample />} />
+          <Route path="/signup" element={<Sample />} />
+          <Route path="/hotelupload" element={<Sample />} />
+          <Route path="/room" element={<Sample />} />
+          <Route path="/payment" element={<Sample />} />
+          <Route path="/admin" element={<Sample />} />
+          <Route path="/adminroute" element={<Sample />} />
+          <Route path="/hotel-details/:id" element={<Sample />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/Sample.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Sample() {
+  return (
+    <div>
+      this is a sample page
+    </div>
+  )
+}
